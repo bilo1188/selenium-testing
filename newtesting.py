4d55c05e1e0473c7b73081c5baf851f6669a7cbe
MUHAMMAD BILAL SIDDIQUI <63120839+bilo1188@users.noreply.github.com>
2020-09-30 16:16:54 +0500

    Add files via upload
    Incomplete script 
    Radio button not working through a script

---
+++ newtesting.py
@@ -0,0 +1,26 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+
+class PythonOrgSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome("C:/Users/BILAL/AppData/Local/Programs/Python/Python37/Scriptschromedriver.exe")
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        driver.implicitly_wait("6000")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+        time.sleep(100)
+
+
+if __name__ == "_main_":
+    unittest.main()
